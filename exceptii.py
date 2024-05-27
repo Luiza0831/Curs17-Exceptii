a4c0f29e53b47c0af791519d8cf7df9c1e8b165c
Luiza Alexandru <alexandru97luiza@yahoo.com>
2024-05-27 19:36:34 +0300

    Problema 6

--- exceptii.py
+++ exceptii.py
@@ -5,3 +5,15 @@ def lungimea_unui_text():
     except ValueError as e:
         print(e)
         return "Introduceti un string"
+    
+dict={"a":1,
+      "b":2,
+      "c":3
+      }
+
+def cheie_valoare(cheie):
+    try:
+        return dict[cheie]
+    except KeyError as e:
+        print(e)
+        return f"Introduceti o cheie valida din dictionarul {dict.keys()}"
